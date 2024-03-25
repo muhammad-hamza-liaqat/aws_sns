1c636ac6dac96902ebc01696d67ac810b6aa0ef0
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-03-25 11:40:01 +0500

    aws.controller.js

--- controller/aws.controller.js
+++ controller/aws.controller.js
@@ -1,6 +1,7 @@
 const aws = require("aws-sdk");
+
 aws.config.update({
-  accessKeyID: process.env.accessKeyID,
+  accessKeyId: process.env.accessKeyID,
   secretAccessKey: process.env.secretAccessKey,
   region: process.env.region,
 });
@@ -8,36 +9,34 @@ aws.config.update({
 const sns = new aws.SNS();
 
 const createTopic = async (req, res) => {
-  //   res.send("hello from createTopic");
   const { topicName } = req.body;
   const params = {
     Name: topicName,
   };
   sns.createTopic(params, (error, data) => {
     if (error) {
-      console.error("error creating sns topic", error);
-      res.status(500).json({ error: "error creating sns topic" });
+      console.error("Error creating SNS topic:", error);
+      res.status(500).json({ error: "Error creating SNS topic" });
     } else {
-      console.log("sns topic created successfully!".data);
+      console.log("SNS topic created successfully!");
       res.json({ topicArn: data.TopicArn });
     }
   });
 };
 
 const pushNotification = async (req, res) => {
-  //   res.send("hello from pushNotification");
   const { message, topicArn } = req.body;
   const params = {
-    message: message,
-    topicArn: topicArn,
+    Message: message,
+    TopicArn: topicArn,
   };
   sns.publish(params, (err, data) => {
     if (err) {
-      console.error("error publishing message", err);
-      res.status(500).json({ message: "error publishing message" });
+      console.error("Error publishing message:", err);
+      res.status(500).json({ error: "Error publishing message" });
     } else {
-      console.log("message published successfully!");
-      res.json({ message: "message published successfully! :) " });
+      console.log("Message published successfully!");
+      res.json({ message: "Message published successfully! :)" });
     }
   });
 };
